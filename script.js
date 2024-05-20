fbe6acc680f6ea567404375b4a2cffbc50d5f5ed
Kumar Shahil <120927231+shahil26@users.noreply.github.com>
2024-05-20 21:47:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+function toggleMenu() {
+    document.getElementById("nav-links").classList.toggle("active");
+  }
+class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+      this.txtElement = txtElement;
+      this.words = words;
+      this.txt = '';
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 8);
+      this.type();
+      this.isDeleting = false;
+    }
+
+    type() {
+      // Current index of word
+      const current = this.wordIndex % this.words.length;
+      // Get full text of current word
+      const fullTxt = this.words[current];
+
+      // Check if deleting
+      if(this.isDeleting) {
+        // Remove char
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        // Add char
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      // Insert txt into element
+      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+      // change color for data-text
+      this.txtElement.innerHTML = `<span class="txt" style="color: #e2000f;">${this.txt}</span>`;
+
+      // Initial Type Speed
+      let typeSpeed = 100;
+
+      if(this.isDeleting) {
+        typeSpeed /= 2;
+      }
+
+      // If word is complete
+      if(!this.isDeleting && this.txt === fullTxt) {
+        // Make pause at end
+        typeSpeed = this.wait;
+        // Set delete to true
+        this.isDeleting = true;
+      } else if(this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        // Move to next word
+        this.wordIndex++;
+        // Pause before start typing
+        typeSpeed = 300;
+      }
+
+      setTimeout(() => this.type(), typeSpeed);
+    }
+  }
+
+  // Init On DOM Load
+  document.addEventListener('DOMContentLoaded', init);
+
+  // Init App
+  function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            alert('Your message has been sent!');
+            form.reset(); // Reset the form fields
+        } else {
+            response.json().then(data => {
+                alert('Oops! Something went wrong.');
+            });
+        }
+    }).catch(error => {
+        alert('Oops! Something went wrong.');
+    });
+}
